b4b628eb3ccb473f2f71aa1b541a0715fc5113f2
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2021-01-21 20:03:08 -0600

    clean up

--- src/SocialQ.Forms/Stores/StoreDetail.xaml.cs
+++ src/SocialQ.Forms/Stores/StoreDetail.xaml.cs
@@ -18,8 +18,7 @@ namespace SocialQ.Forms.Stores
         {
             InitializeComponent();
 
-            this.WhenPropertyChanges(x => x.ViewModel!.StoreId)
-                .Select(x => x.value)
+            this.WhenPropertyValueChanges(x => x.ViewModel!.StoreId)
                 .Where(x => x != Guid.Empty)
                 .InvokeCommand(this, x => x.ViewModel!.InitializeData)
                 .DisposeWith(PageDisposables);
